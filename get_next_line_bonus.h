4c0ce1c97ca404d4b7ea1e536e46f4749c60bb5b
Tsubasa Ishikawa <tsishika@c1r26s4.42tokyo.jp>
2023-06-10 12:38:45 +0900

    add : get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 15:31:43 by tsishika          #+#    #+#             */
-/*   Updated: 2023/06/10 10:10:09 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/06/10 12:38:02 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@
 # include <unistd.h>
 # include <limits.h>
 
-char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
